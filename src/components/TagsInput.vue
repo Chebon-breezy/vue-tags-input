00e70bab91dbc243378a186e3a06ff6f945874d0
Brian Chebon <brianlchebon@gmail.com>
2024-04-18 11:47:29 +0300

    added removetag and removeLasttag methods

--- src/components/TagsInput.vue
+++ src/components/TagsInput.vue
@@ -1,7 +1,15 @@
 <template>
 
-    <div v-for="(tag, index) in tags">{{ index + ":" + tag }}</div>
-    <input type="text" v-bind:value="newTag" />
+    <div v-for="(tag, index) in tags" :key="index">
+        {{ index + ":" + tag }}
+        <a v-on:click.prevent="removeTag(index)" href="#">&times;</a>
+    </div>
+
+    <hr />
+    {{ newTag }}
+
+    <input type="text" v-model.trim="newTag" v-on:keydown.enter="addNewTag" v-on:keydown.delete="removeLastTag"
+        v-on:keydown.tab.prevent="addNewTag" />
 </template>
 
 
@@ -10,6 +18,23 @@ export default {
     data: () => ({
         tags: ["Vue", "React", "Angular", "Next"],
         newTag: "preact"
-    })
-}    
+    }),
+    methods: {
+        addNewTag() {
+
+            if (this.newTag) {
+                this.tags.push(this.newTag);
+                this.newTag = "";
+            }
+        },
+        removeTag(index) {
+            this.tags.splice(index, 1);
+        },
+        removeLastTag() {
+            if (this.newTag.length === 0) {
+                this.removeTag(this.tags.length - 1);
+            }
+        }
+    }
+};    
 </script>
