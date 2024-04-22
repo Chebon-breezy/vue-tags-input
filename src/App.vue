9550c421be131ef65c9fd2bf8916af040b60b292
Brian Chebon <brianlchebon@gmail.com>
2024-04-22 10:07:58 +0300

    created composition api branch

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,9 @@
 <script>
-import TagsInput from "./components/TagsInput.vue";
 
-export default {
-  data: () => ({
-    jsFrameworks: ["react.js", "Vue.js", "Angular.js"],
-    backendFramework: []
-  }),
-
-  methods: {
-    handleChange(tags) {
-      this.jsFrameworks = [...tags];
-    }
-  }
-};
 </script>
 
 <template>
-  <h1>Your Favourite Frameworks</h1>
-  <div>{{ jsFrameworks }}</div>
-  <tags-input :selected-tags="jsFrameworks" @change="handleChange" />
-
-  <div>{{ backendFramework }}</div>
-  <tags-input :selected-tags="backendFramework" @change="backendFramework = [...$event]" />
+  <h1>Hello World</h1>
 </template>
 
-<style>
-h1 {
-  color: blueviolet;
-}
-</style>
+<style scoped></style>
