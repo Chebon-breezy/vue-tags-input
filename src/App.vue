b5ca1a76cd97917bb6f0f1f246cf95dd1de35518
Brian Chebon <brianlchebon@gmail.com>
2024-04-22 13:04:46 +0300

    added ref and reactive functions

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,42 @@
 <script>
+import { ref, reactive } from 'vue'
+
+export default {
+  setup() {
+    const message = ref("Hello");
+    const quantity = ref(1);
+    const item = reactive({
+      name: "product 1",
+      price: 100,
+    })
+
+    const increment = () => quantity.value++;
+    const decrement = () => quantity.value--;
+
+    const swapProduct = () => {
+      item.name = "product A"
+      item.price = 200
+    }
+
+    return {
+      message,
+      quantity,
+      increment,
+      decrement,
+      item,
+      swapProduct
+    };
+  }
+}
 
 </script>
 
 <template>
-  <h1>Hello World</h1>
+  <h1>{{ item.name }} : {{ item.price }}</h1>
+  <button @click="swapProduct">Swap Product</button>
+  <h2>{{ quantity }}</h2>
+  <button @click="increment">+</button>
+  <button @click="decrement">-</button>
 </template>
 
 <style scoped></style>
